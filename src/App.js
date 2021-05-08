2e6cebee7f944469a2fbfcca280467716e1473d4
Bonie Sachdev <bonysachdev99@gmail.com>
2021-05-08 15:52:53 +0530

    Project Page Updated (Responsive)

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import {Route} from 'react-router-dom';
 import SkillsPage from './Pages/SkillsPage';
 import ProjectPage from './Pages/ProjectPage';
 import ContactPage from './Pages/ContactPage';
+import { useState } from 'react';
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () =>{
+    setNavToggle(!navToggle)
+  }
   return (
     <div className="App">
-      <div className="sidebar">
+      <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
         <Navbar />
+      </div>
+      <div className="nav-btn" onClick={navClick}>
+        <div className="lines-1"></div>
+        <div className="lines-2"></div>
+        <div className="lines-3"></div>
       </div>
       <div className="main-content">
         <div className="content">
